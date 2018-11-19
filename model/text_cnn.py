32f1a8746437bbdec4808a2cc297a4c1ec3b7b39
宮沢ともか <miyazawa_tomoka@yahoo.co.jp>
2018-11-19 16:42:44 +0800

    text_cnn add wordembedding

--- model/text_cnn.py
+++ model/text_cnn.py
@@ -10,12 +10,13 @@ class TextCNN(nn.Module):
     A CNN for text classification.
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
     """
-    def __init__(self):
+    def __init__(self, pretrained_weight):
         super(TextCNN, self).__init__()
+        self.with_embedding = False
         in_channel = 1
         out_channel = Config.CNN_KERNEL_NUM
         kernel_sizes = [3, 4, 5]
-
+        self.embedding = nn.Embedding.from_pretrained(pretrained_weight, freeze=False)
         self.conv = nn.ModuleList([nn.Conv2d(in_channel, out_channel, (K, Config.EMBEDDING_SIZE)) for K in kernel_sizes])
 
         self.dropout = nn.Dropout(Config.CNN_DROPOUT)
@@ -27,11 +28,10 @@ class TextCNN(nn.Module):
         :param input_x: a list size having the number of batch_size elements with the same length
         :return: batch_size X num_aspects tensor
         """
-
-        # x = Variable(input_x)
+        x = self.embedding(input_x)
 
         # Conv & max pool
-        x = input_x.unsqueeze(1)  # dim: (batch_size, 1, max_seq_len, embedding_size)
+        x = x.unsqueeze(1)  # dim: (batch_size, 1, max_seq_len, embedding_size)
 
         # turns to be a list: [ti : i \in kernel_sizes] where ti: tensor of dim([batch, num_kernels, max_seq_len-i+1])
         x = [F.relu(conv(x)).squeeze(3) for conv in self.conv]
